c06f31acfe7e006be9cd32c1aec2a8fbf8d05250
Weizhen Sheng <sheng.weizhen@gmail.com>
2019-03-28 16:29:52 -0400

    majority vote

--- homework7.py
+++ homework7.py
@@ -7,7 +7,53 @@ import pandas as pd
 # Part 1 - Majority vote
 
 def majority_vote(mturk_res):
-    pass
+	## Retrieving relevant columns
+	cols = ['Input.attr_id']
+
+	adjstring = 'Input.adj_'
+	ansstring = 'Answer.adj_'
+
+	for i in range(1,11):
+	    cols.append(adjstring + str(i))
+	for i in range(1,11):
+	    cols.append(ansstring + str(i))
+
+	mturk_res = mturk_res[cols]
+	
+	## Creating majority label
+	max_val = int(9 / 3)
+	labels = {}
+
+	# Loop through all attributes
+	for i in range(1,max_val):
+	    temp_df = mturk_res.iloc[(3*(i-1)):(3*i):, :]
+	    attr = temp_df.iloc[0]['Input.attr_id']
+	    
+	    # Loop through all 10 adjectives
+	    for j in range(1,11):
+	        adj = temp_df.iloc[0][input_cols[j]]
+	        label = ''
+	        
+	        # Loop through all 3 HITs
+	        HIT_ans = []
+	        for k in range(3):
+	            HIT_ans.append(temp_df.iloc[k][input_cols[i]])
+	        
+	        if (HIT_ans.count('Yes') > 1):
+	            label = 'Yes'
+	        else:
+	            label = 'No'
+	            
+	        pair = 
+	        labels[(attr,adj)] = label
+
+	## Create output 
+	output_list = []
+	for (k,v) in labels:
+		output_list.append(k[0], k[1], v)
+
+	return sorted(output_list, key=lambda tup: (tup[0],tup[1],tup[2]))
+	# return a list of three-element tuples in the format (attr_id, adj, label) sorted alphabetically given the same column order.
 
 def majority_vote_workers(mturk_res, votes):
     pass
